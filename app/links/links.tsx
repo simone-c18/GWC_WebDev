6ecb3566041c8bc3caca36baf7fa8241063c3195
Isabella <ibellaaustin@gmail.com>
2026-02-06 18:41:15 -0500

    page skeleton

---
+++ app/links/links.tsx
@@ -0,0 +1,30 @@
+const Links = () => {
+  return (
+    <main className="links-page">
+      <section className="links-hero">
+        <h1>Links to all things GWC!</h1>
+        <p>Support us and join our community!</p>
+      </section>
+
+      <section className="links-list">
+        <ul>
+          <li>
+            <a href="#" target="_blank" rel="noopener noreferrer">
+              link
+            </a>
+            <p>link description.</p>
+          </li>
+
+          <li>
+            <a href="#" target="_blank" rel="noopener noreferrer">
+              link
+            </a>
+            <p>link description.</p>
+          </li>
+        </ul>
+      </section>
+    </main>
+  );
+};
+
+export default Links;
