18bacfbc6dd10df4b451aecd9899a47ee06f096a
unknown <nivedita.sujith@gmail.com>
2026-02-21 19:36:38 -0500

    Changed embed link to calendar in page.tsx for update

--- app/calendar/page.tsx
+++ app/calendar/page.tsx
@@ -6,10 +6,10 @@ export default function CalendarPage() {
   return (
     <div>
       <iframe
-        src="https://calendar.google.com/calendar/embed?height=600&wkst=1&ctz=America%2FNew_York&mode=MONTH&showPrint=0&showTitle=0&src=Z3djdWNmZGV2QGdtYWlsLmNvbQ&src=ZDA1YWI3MTM3MTlkZmQzZWE2MzBlOTUzZDgxM2U2ZTY0ODdkZmNlYjI1ZjRlYTgxOWE1ZmJhNTE5ODZlNjA5YkBncm91cC5jYWxlbmRhci5nb29nbGUuY29t&src=ZW4udXNhI2hvbGlkYXlAZ3JvdXAudi5jYWxlbmRhci5nb29nbGUuY29t&color=%23039be5&color=%239e69af&color=%230b8043"
+        src="https://calendar.google.com/calendar/embed?height=500&wkst=1&ctz=America%2FNew_York&title=Girls%20Who%20Code%20Event%20Calendar%20%E2%8B%86%E2%80%A7%C2%B0%F0%93%8F%B2%D6%BC%F0%9D%84%A2&src=Z3djdWNmZGV2QGdtYWlsLmNvbQ&src=ZDA1YWI3MTM3MTlkZmQzZWE2MzBlOTUzZDgxM2U2ZTY0ODdkZmNlYjI1ZjRlYTgxOWE1ZmJhNTE5ODZlNjA5YkBncm91cC5jYWxlbmRhci5nb29nbGUuY29t&src=ZW4udXNhI2hvbGlkYXlAZ3JvdXAudi5jYWxlbmRhci5nb29nbGUuY29t&color=%23b39ddb&color=%23039be5&color=%23b39ddb"
         style={{ border: 0 }}
-        width="800"
-        height="600"
+        width="700"
+        height="500"
       />
     </div>
   );
