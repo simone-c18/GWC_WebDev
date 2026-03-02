d36f93b6ce110443c61f1a733e3eb482bb086a0d
mikaylaphilpot <mikaylagp@hotmail.com>
2026-03-02 12:43:23 -0500

    fix gap to account for header and use global css colors

--- app/join/page.tsx
+++ app/join/page.tsx
@@ -4,10 +4,10 @@ import './join.css';
 
 export default function joinPage() {
     return (
-        <div className="font-alexandria mt-8 flex flex-col break-words justify-center items-center">
+        <div className="font-alexandria mt-28 flex flex-col break-words justify-center items-center">
             {/* Steps Header */}
             <div className="flex flex-col items-center text-center">
-                <div className="text-deep-blue font-bold text-3xl sm:text-5xl">ready to become a member?</div>
+                <div className="text-gwc-darkblue font-bold text-3xl sm:text-5xl">ready to become a member?</div>
                 <div className="font-normal sm:text-base mt-4 text-xs">
                     <div>follow the steps below to become an official member of </div>
                     <div>ucf’s girls who code! no dues are needed :)</div>
@@ -15,7 +15,7 @@ export default function joinPage() {
             </div>
              {/* CollegeLoop Steps */}
             <div className="flex flex-col mx-2 md:ml-4">
-                <div className="md:flex text-deep-blue font-bold text-left text-3xl sm:text-5xl mt-5 sm:mt-10">
+                <div className="md:flex text-gwc-darkblue font-bold text-left text-3xl sm:text-5xl mt-5 sm:mt-10">
                     <div className="whitespace-pre-wrap">→ joining our </div>
                     <div>college loop</div>
                 </div>
@@ -29,7 +29,7 @@ export default function joinPage() {
                     <div>6. fill out your information, and you’re done!</div>
                 </div>
                 {/* KnightConnect Steps */}
-                <div className="text-deep-blue font-bold text-3xl sm:text-5xl mt-5 sm:mt-10">→ joining us on knightsconnect</div>
+                <div className="text-gwc-darkblue font-bold text-3xl sm:text-5xl mt-5 sm:mt-10">→ joining us on knightsconnect</div>
                 <div className="font-normal sm:text-2xl mt-1 sm:mt-4">
                     <p>1. go to <a href="https://knightconnect.campuslabs.com/engage/organizations" className="underline">https://<wbr />knightconnect.campuslabs.com/<wbr />engage/organizations</a></p>
                     <div>2. sign into your ucf account</div>
