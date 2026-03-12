17092d988624c37990c8eda930afcf5503e6848b
Sabplpz <sabrinalpz1994@gmail.com>
2026-03-11 22:56:24 -0400

    added some extra styling setting to the landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,31 +1,35 @@
 export default function Home() {
   return (
-    <main className="min-h-screen bg-white text-foreground flex items-center justify-center">
+    <main className="min-h-screen bg-white text-foreground flex flex-col items-center justify-center gap-8 sm:gap-12 md:gap-16 lg:gap-24">
 
       {/* about us */}
-      <div>
+      <div className="px-6 sm:px-10 md:px-16 lg:px-20">
         <h1 className="text-3xl color-gwc-darkblue font-semibold">
           we're on a mission to close the gender gap in tech
         </h1>
       </div>
 
       {/* about us */}
-      <div>
+      <div className="px-6 sm:px-10 md:px-16 lg:px-20">
 
       </div>
 
       {/* industry */}
-      <div>
+      <div className="px-6 sm:px-10 md:px-16 lg:px-20">
         
       </div>
 
       {/* testimonials */}
-      <div>
+      <div className="px-6 sm:px-10 md:px-16 lg:px-20 font-alexandria bg-[#7b9fd1]">
+
+        <h1 className="text-3xl color-white font-semibold">
+          hear from our community
+        </h1>
         
       </div>
 
       {/* merch */}
-      <div>
+      <div className="px-6 sm:px-10 md:px-16 lg:px-20">
         
       </div>
       
