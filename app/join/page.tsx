81ec2c9a92aa9eae406a2f476a279b7a8f24a35a
mikaylaphilpot <mikaylagp@hotmail.com>
2026-03-14 17:30:31 -0400

    fix issue with container overflow on mobile

--- app/join/page.tsx
+++ app/join/page.tsx
@@ -36,18 +36,18 @@ export default function joinPage() {
                     <div className="font-normal sm:text-2xl  mt-1 sm:mt-4">
                         <div>1. Go to the <a href="https://hq.girlswhocode.com/" target="_blank"><button type="button" className="bg-buttonBlue text-gwc-darkblue font-bold rounded-md border-3 border-gwc-darkblue py-1 px-4 justify-center align-center">Girls Who Code HQ</button></a> website</div>
                         <div className="flex whitespace-pre-wrap">2. click<div className="font-bold text-gwc-darkblue"> ‘college & careers’</div></div>
-                        <div className="flex whitespace-pre-wrap">3. <div className="text-gwc-darkblue font-bold">create</div> or <div className="text-gwc-darkblue font-bold">sign in</div> to your account</div>
-                        <div className="flex whitespace-pre-wrap">4. under the page <div className="text-gwc-darkblue font-bold">‘my programs’</div>, click <div className="text-gwc-darkblue font-bold">‘join new’</div></div>
-                        <div className="flex whitespace-pre-wrap">5. enter our loop code: <div className="text-gwc-darkblue font-bold">CL65361</div></div>
+                        <div className="flex whitespace-pre-wrap">3. <span className="text-gwc-darkblue font-bold">create</span> or <span className="text-gwc-darkblue font-bold">sign in</span> to your account</div>
+                        <div><span className="whitespace-pre-wrap">4. under the page <span className="text-gwc-darkblue font-bold">‘my programs’</span>, click <span className="text-gwc-darkblue font-bold">‘join new’</span></span></div>
+                        <div className="flex whitespace-pre-wrap">5. enter our loop code: <span className="text-gwc-darkblue font-bold">CL65361</span></div>
                         <div>6. fill out your information, and you’re done!</div>
                     </div>
                     {/* KnightConnect Steps */}
                     <div className="text-gwc-darkblue font-bold text-3xl sm:text-5xl mt-5 sm:mt-10">→ join us on knightconnect</div>
                     <div className="font-normal sm:text-2xl mt-1 sm:mt-4">
                         <p>1. Go to <a href="https://knightconnect.campuslabs.com/engage/organizations" target="_blank"><button type="button" className="bg-buttonBlue text-gwc-darkblue font-bold rounded-md border-3 border-gwc-darkblue py-1 px-4">KnightConnect</button></a></p>
-                        <div className="flex whitespace-pre-wrap">2. <div className="text-gwc-darkblue font-bold">sign in</div> to your ucf account</div>
-                        <div className="flex whitespace-pre-wrap">3. in the search bar, <div className="text-gwc-darkblue font-bold">type ‘girls who code’</div></div>
-                        <div className="flex whitespace-pre-wrap">4. <div className="text-gwc-darkblue font-bold">click join</div></div>
+                        <div className="flex whitespace-pre-wrap">2. <span className="text-gwc-darkblue font-bold">sign in</span> to your ucf account</div>
+                        <div className="flex whitespace-pre-wrap">3. in the search bar, <span className="text-gwc-darkblue font-bold">type ‘girls who code’</span></div>
+                        <div className="flex whitespace-pre-wrap">4. <span className="text-gwc-darkblue font-bold">click join</span></div>
                     </div>
                 </div>
             </div>
