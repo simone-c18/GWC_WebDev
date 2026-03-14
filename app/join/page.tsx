851d2dbd37416e2a363b836d287061ea831a5ecb
mikaylaphilpot <mikaylagp@hotmail.com>
2026-03-14 16:34:38 -0400

    change join page to button style formatting

--- app/join/page.tsx
+++ app/join/page.tsx
@@ -29,25 +29,25 @@ export default function joinPage() {
                 {/* CollegeLoop Steps */}
                 <div className="flex flex-col mx-2 md:ml-4">
                     <div className="md:flex text-gwc-darkblue font-bold text-left text-3xl sm:text-5xl mt-5 sm:mt-10">
-                        <div className="whitespace-pre-wrap">→ joining our </div>
+                        <div className="whitespace-pre-wrap">→ join us on </div>
                         <div>college loop</div>
                     </div>
                     
                     <div className="font-normal sm:text-2xl  mt-1 sm:mt-4">
-                        <div>1. go to <a href="https://hq.girlswhocode.com/" className="underline">https://hq.girlswhocode.com/</a></div>
-                        <div>2. click ‘college & careers’</div>
-                        <div>3. create or sign in to your account</div>
-                        <div>4. under the page ‘my programs’, click ‘join new’</div>
-                        <div>5. enter our loop code: CL65361</div>
+                        <div>1. Go to the <a href="https://hq.girlswhocode.com/" target="_blank"><button type="button" className="bg-buttonBlue text-gwc-darkblue font-bold rounded-md border-3 border-gwc-darkblue py-1 px-4 justify-center align-center">Girls Who Code HQ</button></a> website</div>
+                        <div className="flex whitespace-pre-wrap">2. click<div className="font-bold text-gwc-darkblue"> ‘college & careers’</div></div>
+                        <div className="flex whitespace-pre-wrap">3. <div className="text-gwc-darkblue font-bold">create</div> or <div className="text-gwc-darkblue font-bold">sign in</div> to your account</div>
+                        <div className="flex whitespace-pre-wrap">4. under the page <div className="text-gwc-darkblue font-bold">‘my programs’</div>, click <div className="text-gwc-darkblue font-bold">‘join new’</div></div>
+                        <div className="flex whitespace-pre-wrap">5. enter our loop code: <div className="text-gwc-darkblue font-bold">CL65361</div></div>
                         <div>6. fill out your information, and you’re done!</div>
                     </div>
                     {/* KnightConnect Steps */}
-                    <div className="text-gwc-darkblue font-bold text-3xl sm:text-5xl mt-5 sm:mt-10">→ joining us on knightsconnect</div>
+                    <div className="text-gwc-darkblue font-bold text-3xl sm:text-5xl mt-5 sm:mt-10">→ join us on knightconnect</div>
                     <div className="font-normal sm:text-2xl mt-1 sm:mt-4">
-                        <p>1. go to <a href="https://knightconnect.campuslabs.com/engage/organizations" className="underline">https://<wbr />knightconnect.campuslabs.com/<wbr />engage/organizations</a></p>
-                        <div>2. sign into your ucf account</div>
-                        <div>3. in the search bar, type ‘girls who code’</div>
-                        <div>4. click join</div>
+                        <p>1. Go to <a href="https://knightconnect.campuslabs.com/engage/organizations" target="_blank"><button type="button" className="bg-buttonBlue text-gwc-darkblue font-bold rounded-md border-3 border-gwc-darkblue py-1 px-4">KnightConnect</button></a></p>
+                        <div className="flex whitespace-pre-wrap">2. <div className="text-gwc-darkblue font-bold">sign in</div> to your ucf account</div>
+                        <div className="flex whitespace-pre-wrap">3. in the search bar, <div className="text-gwc-darkblue font-bold">type ‘girls who code’</div></div>
+                        <div className="flex whitespace-pre-wrap">4. <div className="text-gwc-darkblue font-bold">click join</div></div>
                     </div>
                 </div>
             </div>
