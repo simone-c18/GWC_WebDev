ab1cb62d485f860409e530c56a5fbcec48e522ac
simone-c18 <simone.chrastek5@gmail.com>
2026-04-17 00:48:45 -0400

    minor ui fixes

--- components/landing/Merch.tsx
+++ components/landing/Merch.tsx
@@ -6,7 +6,7 @@ export default function Merch() {
     <section className="relative w-[95%] bg-[#7b9fd1] lg:min-h-[700px] max-w-7xl mx-auto rounded-2xl flex flex-col md:flex-row items-stretch overflow-hidden">
 
       {/* Image */}
-      <div className="relative w-full md:w-[67%] h-[150px] sm:h-[150px] md:h-auto shrink-0 p-4 md:p-6 lg:p-7 xl:p-8">
+      <div className="relative w-full md:w-[45%] h-[250px] sm:h-[350px] md:h-auto shrink-0 p-4 md:p-6 lg:p-7">
         <div className="relative w-full h-full overflow-hidden rounded-2xl">
           <Image
             src='/images/gwcmerchupscaled.png'
@@ -18,13 +18,13 @@ export default function Merch() {
       </div>
 
       {/* Text */}
-      <div className="flex flex-col items-center md:items-start justify-around text-center md:text-left px-6 sm:px-8 md:px-10 pt-2 pb-8 md:py-12">
-        <h1 className="text-4xl sm:text-5xl xl:text-6xl text-gwc-darkblue font-semibold leading-snug mb-4">
-          merch by us, <br/>
+      <div className="flex flex-col items-center md:items-start justify-center text-center md:text-left px-6 sm:px-8 md:px-10 py-10">
+        <h1 className="text-3xl sm:text-4xl md:text-5xl text-white font-semibold leading-snug mb-6">
+          merch by us, 
           for you <br/>
         </h1>
-        <p className="text-lg md:text-xl lg:text-2xl xl:text-3xl text-white mb-3">
-          support ucf’s girls who code through stickers, sweaters, and more!
+        <p className="text-base md:text-lg lg:text-xl text-white mb-8">
+          support ucf's girls who code through stickers, sweaters, and more!
         </p>
         <Link
           href="https://docs.google.com/forms/d/e/1FAIpQLSdrsTA0wyEK27GAf3yfECBmk5DtUCPHIzREzUgEnXbORAMWdQ/viewform"
