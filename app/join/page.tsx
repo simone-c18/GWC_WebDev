5dd9765ad9a624f4a8478c4ef5e3bdda09414c98
mikaylaphilpot <mikaylagp@hotmail.com>
2026-05-06 14:12:38 -0400

    fix text sizing issues
    looked better smaller instead of larger

--- app/join/page.tsx
+++ app/join/page.tsx
@@ -21,7 +21,7 @@ export default function joinPage() {
                         <div>college loop</div>
                     </div>
                     
-                    <div className="font-normal sm:text-xl md:text-2xl  mt-1 sm:mt-4">
+                    <div className="font-normal sm:text-xl mt-1 sm:mt-4">
                         <div className="flex whitespace-pre-wrap mt-4 align-center flex-wrap"><div>1. Go to the   </div><a href="https://hq.girlswhocode.com/" target="_blank"><button type="button" className="bg-buttonBlue text-gwc-darkblue font-bold rounded-md border-3 border-gwc-darkblue py-1 px-4 justify-center align-center text-md sm:text-xl md:text-2xl lg:txt-3xl -mt-2 md:-mt-2">Girls Who Code HQ</button></a><div>   website</div></div>
                         <div className="flex whitespace-pre-wrap mt-2 md:mt-4">2. click<div className="font-bold text-gwc-darkblue"> ‘college & careers’</div></div>
                         <div className="flex whitespace-pre-wrap">3. <span className="text-gwc-darkblue font-bold">create</span> or <span className="text-gwc-darkblue font-bold">sign in</span> to your account</div>
@@ -31,7 +31,7 @@ export default function joinPage() {
                     </div>
                     {/* KnightConnect Steps */}
                     <div className="text-gwc-darkblue font-bold text-3xl sm:text-5xl mt-5 sm:mt-10">→ join us on knightconnect</div>
-                    <div className="font-normal sm:text-xl md:text-2xl mt-1 sm:mt-4">
+                    <div className="font-normal sm:text-xl mt-1 sm:mt-4">
                         <p className="flex whitespace-pre-wrap mt-4 align-center flex-wrap">1. Go to   <a href="https://knightconnect.campuslabs.com/engage/organizations" target="_blank"><button type="button" className="bg-buttonBlue text-gwc-darkblue font-bold rounded-md border-3 border-gwc-darkblue py-1 px-4 justify-center align-center text-md sm:text-xl md:text-2xl lg:txt-3xl -mt-2 md:-mt-2">KnightConnect</button></a></p>
                         <div className="flex whitespace-pre-wrap mt-2 md:mt-4">2. <span className="text-gwc-darkblue font-bold">sign in</span> to your ucf account</div>
                         <div className="flex whitespace-pre-wrap">3. in the search bar, <span className="text-gwc-darkblue font-bold">type ‘girls who code’</span></div>
